0f127f6376649afe0673ff4d1797f72410a02547
Sowjanya046 <74344630+Sowjanya046@users.noreply.github.com>
2026-04-27 23:03:41 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,28 @@
+#lists
+list = ['sowjanya', 'Automotive','System']
+print(list) #print the list
+
+#lenth of list
+print(len(list))
+
+#Index ihn List
+print(list[1])
+
+#last value in index
+print(list[2])
+
+#negative list
+print(list[-1])
+print(list[-2])
+print(list[-3])
+
+#range of values
+print(list[0:3])
+
+#add an item in list
+list.append('Engineer')
+print(list)
+
+#insert an item in list
+list.insert(1,'Engineer')
+print(list)
